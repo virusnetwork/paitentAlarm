07fa943aaa1ba0a6f6dfe29af74f85851b60266b
Miles Singleton <milessingleton@hotmail.co.uk>
2021-01-11 10:06:33 +0000

    set up function for speech to microphone

--- main.py
+++ main.py
@@ -4,39 +4,53 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import speech_recognition as sr
 
+
 def speech_from_mic(recognizer, microphone):
     """takes speech from microphone turn to text
     returns a directory with one of 3 values
-    "success" : boolean saying API request was sucessful or not
-    "error": none if no errors otherwise a string containg the error
-    "transcription": none if speech could not be transcibed else a string
+    "success" : boolean saying API request was successful or not
+    "error": none if no errors otherwise a string containing the error
+    "transcription": none if speech could not be transcribed else a string
     """
 
-    #check that recognizer and mariphone are appropriate types
-    if not isinstance(recognizer,sr.Recognizer):
+    # check that recognizer and microphone are appropriate types
+    if not isinstance(recognizer, sr.Recognizer):
         raise TypeError("`recognizer` must be `Recognizer` instance")
 
     if not isinstance(microphone, sr.Microphone):
         raise TypeError("microphone object must be of sr.Microphone")
 
-    #we ajust amibant sensinstanty to ambient noise
-    #then we record from microphone and save as var audio
+    # we adjust ambient sensitivity to ambient noise
+    # then we record from microphone and save as var audio
     with microphone as source:
         recognizer.adjust_for_ambient_noise(source)
         audio = recognizer.listen(source)
 
-    #seting up response object
+    # setting up response object
     response = {
         "success": True,
         "error": None,
         "transcription": None
     }
 
-    
+    # try recognizing the speech in the recoding (audio)
+    # if a RequestError or unknown value error exception is caught,
+    #   update the response object
+    try:
+        response["transcription"] = recognizer.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was untranslatable
+        response["error"] = "Unable to recognize speech"
+
+    return response
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
+    pass
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
